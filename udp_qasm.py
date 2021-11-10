60e1a289bd9dad3322903ff3f0354d7afc88b4ac
Itaborala <28213905+Itaborala@users.noreply.github.com>
2021-11-10 17:36:42 -0300

    Adding qasm_simulator feature
    This fixes #3

--- udp_qasm.py
+++ udp_qasm.py
@@ -8,13 +8,31 @@
 
 from pythonosc import dispatcher, osc_server, udp_client
 from qiskit import *
-import random
 import sys
+
+def run_circuit(qc, shots=1000, backend_name='qasm_simulator', provider=Aer):
+
+    backend = provider.get_backend(backend_name)
+    print("Running circuit on {}...".format(backend_name))
+    job = execute(qc, shots=shots, backend=backend)
+    print("Done!")
+    return job.result().get_counts()
 
 def parse_qasm(*args):
     global qc
     qc=QuantumCircuit().from_qasm_str(args[1])
-    client.send_message("/QuTune", random.random())
+    counts = run_circuit(qc)
+    print("Sending result counts back to Max")
+    # list comprehension that converts a Dict into an
+    # interleaved string list: [key1, value1, key2, value2...]
+    sorted_counts = {}
+    for key in sorted(counts):
+        #print ("%s: %s" % (key, counts[key]) )
+        sorted_counts[key]=counts[key]
+    counts_list = [str(x) for z in zip(sorted_counts.keys(), sorted_counts.values()) for x in z]
+    # and then into a string
+    counts_list = " ".join(counts_list)
+    client.send_message("counts", counts_list)
 
 def main(UDP_IP="127.0.0.1", RECEIVE_PORT=1416, SEND_PORT=1417):
     global client
