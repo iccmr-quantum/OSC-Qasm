722dbba854caa5c9e02a17c925369c8844022e5f
Itaborala <28213905+Itaborala@users.noreply.github.com>
2021-11-10 21:26:41 -0300

    job_monitor hotfix#2

--- udp_qasm.py
+++ udp_qasm.py
@@ -14,18 +14,17 @@ import argparse
 
 def run_circuit(qc, shots, backend_name):
 
+    print("Running circuit on {}...".format(backend_name))
     if backend_name != 'qasm_simulator':
         if not provider:
             client.send_message("error", "You need to start UDP-Qasm with the following arguments: --token (--hub, --group, --project).")
             raise ValueError('You need to start UDP-Qasm with the following arguments: --token (--hub, --group, --project).')
         backend = provider.get_backend(backend_name)
         job = execute(qc, shots=shots, backend=backend)
+        job_monitor(job)
     else:
         backend = Aer.get_backend('qasm_simulator')
         job = execute(qc, shots=shots, backend=backend)
-    print("Running circuit on {}...".format(backend_name))
-    #job = execute(qc, shots=shots, backend=backend)
-    job_monitor(job)
     print("Done!")
     return job.result().get_counts()
 
